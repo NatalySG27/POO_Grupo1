e8a034f668e74a637621c7f6e73cddbc9613bdec
Anderson <andersoncruzadoalvarez64@gmail.com>
2026-09-18 19:02:52 -0500

    Cambio de Anderson

--- Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package prácticas_poo_grupo1;
 
-/**
- *
- * @author natal
- */
 public class Prácticas_POO_Grupo1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Trabajando en la semana 2");
+        System.out.println("Cambio de Anderson listo");
     }
     
 }
