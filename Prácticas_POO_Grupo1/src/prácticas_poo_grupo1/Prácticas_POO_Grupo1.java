34857ad4f17e5dd2149fde2842cd121fa02c1013
Cristofer <n00423619@upn.pe>
2026-09-20 14:27:14 -0500

    cambio cristofer

--- Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
@@ -7,6 +7,7 @@ public class Prácticas_POO_Grupo1 {
         // TODO code application logic here
         System.out.println("Trabajando en la semana 2");
         System.out.println("Cambio de Anderson listo");
+        System.out.println("cambio de cristofer");
     }
     
 }
