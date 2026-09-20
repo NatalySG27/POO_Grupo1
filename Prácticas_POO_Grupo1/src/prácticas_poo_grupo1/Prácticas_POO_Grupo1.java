0a59b14fdf20b9497aa4e9d29deed651768ae1ce
Angel Daniel <yersonsaenz345@gmail.com>
2026-09-19 20:59:48 -0500

    cristofer_cambio

--- Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
@@ -7,7 +7,7 @@ public class Prácticas_POO_Grupo1 {
         // TODO code application logic here
         System.out.println("Trabajando en la semana 2");
         System.out.println("Cambio de Anderson listo");
-        System.out.println("Cambio de Cristofer listo");
+        System.out.println("Cambio de Cristofer listo.");
 
     }
     
