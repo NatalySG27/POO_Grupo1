37a890967fd2fb63a88cb1a2bb54091332120e11
Nataly Solano <N00462738@upn.pe>
2026-09-24 01:06:57 -0500

    Creacion de la clase Persona y sus atributos base

---
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Persona.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prácticas_poo_grupo1;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author natal
+ */
+public class Persona {
+    String tipo_documento;
+    String nro_documento;
+    String nombre;
+    String paterno;
+    String materno;
+    LocalDate fecha_nacimiento;
+}
