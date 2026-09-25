2ecd1c34bb872f40f912d11206b4943d1d2cb7b3
calderonjheyson102-rgb <calderonjheyson102@gmail.com>
2026-09-24 22:29:13 -0500

    Actualizar clase principal

--- Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
@@ -6,7 +6,7 @@ public class Prácticas_POO_Grupo1 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Trabajando en la semana 2");
+        System.out .println("Trabajando en la semana 2");
         System.out.println("Cambio de Anderson listo");
         
         System.out.println("cambio realizado");
