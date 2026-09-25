05750f63e37cd6d9fff8b481d0d0ff6bb24fcac1
calderonjheyson102-rgb <calderonjheyson102@gmail.com>
2026-09-24 22:15:16 -0500

    Resolver conflicto de Git

--- Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/Prácticas_POO_Grupo1.java
@@ -1,3 +1,21 @@
+<<<<<<< OURS
+
+package prácticas_poo_grupo1;
+
+public class Prácticas_POO_Grupo1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Trabajando en la semana 2");
+        System.out.println("Cambio de Anderson listo");
+        
+        System.out.println("cambio realizado");
+
+        
+    }
+    
+}
+=======
 
 package prácticas_poo_grupo1;
 
@@ -11,3 +29,4 @@ public class Prácticas_POO_Grupo1 {
     
 }
 // Prueba de cambio para Git
+>>>>>>> THEIRS
