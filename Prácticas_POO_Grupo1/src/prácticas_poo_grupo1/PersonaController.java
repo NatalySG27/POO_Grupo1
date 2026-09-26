ec589b796488748d28d53d9ea21d65c4f5be1bd1
Cristofer <n00423619@upn.pe>
2026-09-25 23:43:04 -0500

    Agregado PersonaController y lista inicial

---
+++ Prácticas_POO_Grupo1/src/prácticas_poo_grupo1/PersonaController.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prácticas_poo_grupo1;
+
+import java.util.ArrayList;
+
+public class PersonaController {
+    
+    ArrayList<Persona> lista = new ArrayList<>();
+
+    public void agregarPersona(Persona nuevapersona) {
+        lista.add(nuevapersona);
+    }
+}
